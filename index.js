0a8fea14244a82e292cecf7833fcce5f7cc710fb
Sheheryar Waseem <sherishaikh6@gmail.com>
2022-04-28 16:28:03 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const bodyParser = require ('body-parser')
+const api = require('./src/api')
+
+const app = express()
+const PORT = 5000
+
+app.use(bodyParser.json());
+
+app.use('api/v1', api )
+
+
+
+
+app.listen(PORT, () => 
+    console.log(`Server is running on port ${PORT}`))
